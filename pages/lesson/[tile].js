a4f8476402990b3854afd07ac0ef0de8ea6c9788
Patrick Burns <burns212@gmail.com>
2022-08-30 20:42:11 -0500

    terminal is satan

--- pages/lesson/[tile].js
+++ pages/lesson/[tile].js
@@ -1,52 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+// import React, { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
 import TopNavigation from '../../components/TopNavigation';
-import { useAuth } from '../../utils/context/authContext';
-import {
-  createScore, getScoreFirebaseKeysByUid, getScoresByUid, updateScoreByFirebaseKey,
-} from '../../api/scores';
+// import { useAuth } from '../../utils/context/authContext';
+// import {
+//   createScore, getScoreFirebaseKeysByUid, getScoresByUid, updateScoreByFirebaseKey,
+// } from '../../api/scores';
 import questions from '../../sampleData/questions.json';
 
-const initialState = {
-  score: '',
-};
+// const initialState = {
+//   score: '',
+// };
 
 export default function Lesson() {
-  const { user } = useAuth();
-  const router = useRouter();
-  const { tile } = router.query;
-  const [scores, setScores] = useState({});
-  const [firebaseKeys, setFirebaseKeys] = useState({});
-  const [formInput, setFormInput] = useState(initialState);
+  // const { user } = useAuth();
+  // const router = useRouter();
+  // const { tile } = router.query;
+  // const [scores, setScores] = useState({});
+  // const [firebaseKeys, setFirebaseKeys] = useState({});
+  // const [formInput, setFormInput] = useState(initialState);
   const questionNumber = 0;
-  useEffect(() => {
-    getScoresByUid(user.uid).then(setScores);
-    getScoreFirebaseKeysByUid(user.uid).then(setFirebaseKeys);
-  }, [user.uid]);
+  // useEffect(() => {
+  //   getScoresByUid(user.uid).then(setScores);
+  //   getScoreFirebaseKeysByUid(user.uid).then(setFirebaseKeys);
+  // }, [user.uid]);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormInput((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (scores[tile]) {
-      const payload = {
-        score: Math.ceil((parseInt(scores[tile], 10) + parseInt(formInput.score, 10)) / 2),
-      };
-      updateScoreByFirebaseKey(firebaseKeys[tile], payload).then(() => router.push('/'));
-    } else {
-      const payload = {
-        tile,
-        uid: user.uid,
-        score: Math.ceil((parseInt(formInput.score, 10)) / 2),
-      };
-      createScore(payload).then(() => router.push('/'));
-    }
-  };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormInput((prevState) => ({
+  //     ...prevState,
+  //     [name]: value,
+  //   }));
+  // };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   if (scores[tile]) {
+  //     const payload = {
+  //       score: Math.ceil((parseInt(scores[tile], 10) + parseInt(formInput.score, 10)) / 2),
+  //     };
+  //     updateScoreByFirebaseKey(firebaseKeys[tile], payload).then(() => router.push('/'));
+  //   } else {
+  //     const payload = {
+  //       tile,
+  //       uid: user.uid,
+  //       score: Math.ceil((parseInt(formInput.score, 10)) / 2),
+  //     };
+  //     createScore(payload).then(() => router.push('/'));
+  //   }
+  // };
 
   return (
     <div className="container1">
